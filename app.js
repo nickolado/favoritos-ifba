de62ede5433b854d941c32cb434904d3b7aa6269
Daniel Richard <daniel.anjos@lwsa.tech>
2026-05-08 20:50:55 -0300

    criado favoritos

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express")
 const app = express()
 const port = 8000
 const rotaLivro = require("./rotas/livro")
+const rotaFavorito = require("./rotas/favorito")
 
 // para usar o body
 app.use(express.json())
 
 app.use('/livros', rotaLivro)
+app.use('/favoritos', rotaFavorito)
 
 
 app.listen(port, () => {
